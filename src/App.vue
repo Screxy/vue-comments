4617d8a43615d7d179eaae1d2c503f755fec1449
Screxy <dvbvladis@mail.ru>
2023-06-03 16:30:08 +0300

    maked 2 components

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,115 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <header>
+    </header>
+    <main>
+        <div class="wrapper">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+    export default {
+        data() {
+            return {
+                formData: {
+                    "author": "",
+                    "text": "",
+                    "reaction": null,
+                    "parentId": 0,
+                },
+                comments: [
+                    {
+                        "id": 1,
+                        "author": "Screxy",
+                        "text": "Пост пушка! Рад, что нашел тебя!",
+                        "reaction": 1,
+                        "parentId": 0,
+                        "createdAt": "2023-06-03T12:42:22.398Z"
+                    },
+                    {
+                        "id": 2,
+                        "author": "Nikolya",
+                        "text": "Не согласен",
+                        "reaction": -1,
+                        "parentId": 1,
+                        "createdAt": "2023-06-03T12:42:22.398Z"
+                    },
+                    {
+                        "id": 3,
+                        "author": "Petya",
+                        "text": "Пост объективно не очень",
+                        "reaction": -1,
+                        "parentId": 1,
+                        "createdAt": "2023-06-03T12:42:22.398Z"
+                    },
+                    {
+                        "id": 4,
+                        "author": "Slavaver",
+                        "text": "Пост средний, надо переделать",
+                        "reaction": 0,
+                        "parentId": 0,
+                        "createdAt": "2023-06-03T12:42:22.398Z"
+                    }, {
+                        "id": 5,
+                        "author": "Misha",
+                        "text": "согласен",
+                        "reaction": 1,
+                        "parentId": 4,
+                        "createdAt": "2023-06-03T12:42:22.398Z"
+                    },
+                    {
+                        "id": 6,
+                        "author": "Olya",
+                        "text": "вы правы",
+                        "reaction": 1,
+                        "parentId": 4,
+                        "createdAt": "2023-06-03T12:42:22.398Z"
+                    },
+                ]
+            }
+        },
+        methods: {
+            addComment() {
+                this.comments.push({
+                    "id": this.comments.length,
+                    "author": this.formData.author,
+                    "text": this.formData.text,
+                    "reaction": this.formData.reaction,
+                    "parentId": 0,
+                    "createdAt": this.convertDate
+                })
+                this.formData = {}
+            },
+        },
+        computed: {
+            date() {
+                return Date.now();
+            },
+            convertDate() {
+                return new Intl.DateTimeFormat("ru", {
+                    year: "numeric",
+                    month: "numeric",
+                    day: "numeric",
+                    hour: "numeric",
+                    minute: "numeric",
+                }).format(this.date);
+            },
+        }
+    }
+</script>
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+.wrapper {
+    padding: 20px;
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 }
 </style>
---
+++ src/components/CommentList.vue
@@ -0,0 +1,29 @@
+<template>
+    <ul class="list">
+        <li class="list__item" v-for="comment in comments">
+            <h3>
+                {{ comment.author }}
+            </h3>
+            <p>
+                {{ comment.text }}
+            </p>
+            <p>
+                {{ comment.createdAt }}
+            </p>
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss" scoped>
+.list__item {
+    padding: 15px;
+    border: 3px solid teal;
+    margin-top: 10px;
+}
+</style>
